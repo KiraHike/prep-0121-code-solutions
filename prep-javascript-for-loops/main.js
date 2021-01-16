2eb88320e88f8f98fca837b6b9054c4f76ff74d6
Kira Hike <kirahikes@gmail.com>
2021-01-16 12:36:28 -0800

    completed prep javascript for loops exercise | Kira Hike

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,17 @@
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (i = 0; i < 50; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+for (i = 0; i < 20; i = i + 2) {
+  console.log(i);
+}
+
+for (var x = 100; x > 0; x--) {
+  console.log('Time till explosion:', x);
+}
